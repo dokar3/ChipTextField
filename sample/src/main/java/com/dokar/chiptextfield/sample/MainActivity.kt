4319928c4093bbfb2cf110421057af0041a97e89
dokar3 <awesley@qq.com>
2021-11-10 16:50:10 +0800

    Sample: Add theme color selector

--- sample/src/main/java/com/dokar/chiptextfield/sample/MainActivity.kt
+++ sample/src/main/java/com/dokar/chiptextfield/sample/MainActivity.kt
@@ -3,15 +3,28 @@ package com.dokar.chiptextfield.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dokar.chiptextfield.Chip
@@ -20,6 +33,19 @@ import com.dokar.chiptextfield.rememberChipInputFieldState
 import com.dokar.chiptextfield.sample.theme.ChipTextFieldTheme
 
 private const val LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+
+private data class ChipColors(
+    val text: Color,
+    val border: Color,
+    val background: Color
+)
+
+private val THEME_COLORS = listOf(
+    ChipColors(text = Color.Black, border = Color.Black, background = Color.Transparent),
+    ChipColors(text = Color.White, border = Color(0xff94d2bd), background = Color(0xff94d2bd)),
+    ChipColors(text = Color.White, border = Color(0xffe85d04), background = Color(0xffe85d04)),
+    ChipColors(text = Color.White, border = Color(0xff9fa0ff), background = Color(0xff9fa0ff))
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +63,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SampleScreen(name: String) {
+    val selectedColorPosition = remember {
+        mutableStateOf(0)
+    }
+
+    val chipColors = when (selectedColorPosition.value) {
+        0 -> {
+            getDefaultChipColors()
+        }
+        else -> {
+            THEME_COLORS[selectedColorPosition.value]
+        }
+    }
+
     Column {
         val chips = remember {
             LOREM_IPSUM.split(" ").map(::Chip)
@@ -45,7 +84,74 @@ fun SampleScreen(name: String) {
         ChipTextField(
             state = state,
             modifier = Modifier.fillMaxWidth().padding(8.dp),
-            initialTextFieldValue = name
+            initialTextFieldValue = name,
+            chipTextColor = chipColors.text,
+            chipBorderColor = chipColors.border,
+            chipBackgroundColor = chipColors.background
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        ThemeColorSelector(
+            selectedPosition = selectedColorPosition,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+private fun ThemeColorSelector(
+    selectedPosition: MutableState<Int>,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        for ((index, colors) in THEME_COLORS.withIndex()) {
+            val chipColors = if (index == 0) {
+                getDefaultChipColors()
+            } else {
+                colors
+            }
+            ColorItem(
+                color = chipColors.border,
+                isSelected = index == selectedPosition.value,
+                modifier = Modifier.clickable { selectedPosition.value = index }
+            )
+        }
+    }
+}
+
+@Composable
+private fun ColorItem(
+    color: Color,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier
+) {
+    val borderColor = if (isSelected) {
+        color
+    } else {
+        Color.Transparent
+    }
+    Box(
+        modifier.size(32.dp)
+            .clip(CircleShape)
+            .border(width = 2.dp, color = borderColor, shape = CircleShape)
+            .padding(4.dp)
+            .background(color = color, shape = CircleShape)
+    )
+}
+
+@Composable
+private fun getDefaultChipColors(): ChipColors {
+    return if (MaterialTheme.colors.isLight) {
+        THEME_COLORS[0]
+    } else {
+        ChipColors(
+            text = Color.White,
+            border = Color.White,
+            background = Color.Transparent
         )
     }
 }
