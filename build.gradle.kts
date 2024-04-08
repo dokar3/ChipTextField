886b82d87466aa971d0d2b3c69324e3499ac582e
dokar3 <68095777+dokar3@users.noreply.github.com>
2024-04-08 11:33:37 +0800

    Update Compose Compiler to 1.5.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     val composeBomVersion: String by extra("2024.02.01")
-    val composeCompilerVersion: String by extra("1.5.10")
+    val composeCompilerVersion: String by extra("1.5.11")
 }
 
 plugins {
