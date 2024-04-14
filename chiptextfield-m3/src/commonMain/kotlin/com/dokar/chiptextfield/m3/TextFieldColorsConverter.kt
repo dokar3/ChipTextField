4c51d948a232b1a286eaf9aafffe35e126aca4d0
dokar3 <68095777+dokar3@users.noreply.github.com>
2024-04-14 13:25:18 +0800

    Fix NoSuchMethodError when using material3 v1.3

--- chiptextfield-m3/src/commonMain/kotlin/com/dokar/chiptextfield/m3/TextFieldColorsConverter.kt
+++ chiptextfield-m3/src/commonMain/kotlin/com/dokar/chiptextfield/m3/TextFieldColorsConverter.kt
@@ -2,12 +2,20 @@
 
 package com.dokar.chiptextfield.m3
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.graphics.Color
 import com.dokar.chiptextfield.ChipTextFieldColors
+
+// Copied from material3 v1.2
+private const val AnimationDuration = 150
 
 fun TextFieldColors.toChipTextFieldColors(): ChipTextFieldColors {
     return object : ChipTextFieldColors {
@@ -17,16 +25,19 @@ fun TextFieldColors.toChipTextFieldColors(): ChipTextFieldColors {
             isError: Boolean,
             interactionSource: InteractionSource
         ): State<Color> {
-            return this@toChipTextFieldColors.textColor(
-                enabled = enabled,
-                isError = isError,
-                interactionSource = interactionSource,
-            )
+            val focused by interactionSource.collectIsFocusedAsState()
+            val targetValue = when {
+                !enabled -> disabledTextColor
+                isError -> errorTextColor
+                focused -> focusedTextColor
+                else -> unfocusedTextColor
+            }
+            return rememberUpdatedState(targetValue)
         }
 
         @Composable
         override fun cursorColor(isError: Boolean): State<Color> {
-            return this@toChipTextFieldColors.cursorColor(isError = isError)
+            return rememberUpdatedState(if (isError) errorCursorColor else cursorColor)
         }
 
         @Composable
@@ -35,11 +46,14 @@ fun TextFieldColors.toChipTextFieldColors(): ChipTextFieldColors {
             isError: Boolean,
             interactionSource: InteractionSource
         ): State<Color> {
-            return this@toChipTextFieldColors.containerColor(
-                enabled = enabled,
-                isError = isError,
-                interactionSource = interactionSource,
-            )
+            val focused by interactionSource.collectIsFocusedAsState()
+            val targetValue = when {
+                !enabled -> disabledContainerColor
+                isError -> errorContainerColor
+                focused -> focusedContainerColor
+                else -> unfocusedContainerColor
+            }
+            return animateColorAsState(targetValue, tween(durationMillis = AnimationDuration))
         }
     }
 }
